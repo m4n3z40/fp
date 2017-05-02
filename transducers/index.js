15d681284bbcec88e01df1948c93644021813c67
Allan Marques Baptista <allan.marques@ymail.com>
2017-05-01 21:10:48 -0300

    Changing tranducers index.

--- transducers/index.js
+++ transducers/index.js
@@ -1,1 +1,14 @@
-import mapTransformer from './transformers/mapTransformer';
+export { default as map } from './transformers/mapTransformer';
+export { default as filter } from './transformers/filterTransformer';
+export { default } from './maketransducer';
+import arrayCombiner from './combiners/arrayCombiner';
+import flatArrayCombiner from './combiners/flatArrayCombiner';
+import immutableArrayCombiner from './combiners/immutableArrayCombiner';
+import stringCombiner from './combiners/stringCombiner';
+
+export const combine = {
+    array: arrayCombiner,
+    flatArray: flatArrayCombiner,
+    immutableArray: immutableArrayCombiner,
+    string: stringCombiner
+};
